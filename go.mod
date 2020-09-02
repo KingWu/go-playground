e045b50c86717c44d0de51283b35ce46ffa0be16
King Wu <king.wuff101@gmail.com>
2020-09-02 13:05:37 +0800

    Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KingWu/go-playground
+
+go 1.15
+
+require rsc.io/quote v1.5.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import (
+	"fmt"
+	"rsc.io/quote"
+)
+
+func main() {
+ fmt.Println(fmt.Println(quote.Hello()))
+}
