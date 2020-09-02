d32624da1d3003b39936868d7e918c2daad903ee
King Wu <king.wuff101@gmail.com>
2020-09-02 23:41:52 +0800

    Try path

--- main.go
+++ main.go
@@ -1,9 +1,92 @@
 package main
+
 import (
-	"fmt"
-	"rsc.io/quote"
+    "fmt"
+		"math"
+		"github.com/KingWu/go-playground/src/model"
 )
 
+// A Month specifies a month of the year (January = 1, ...).
+type Month int
+
+// s
+const (
+    January Month = 1 + iota
+    February
+    March
+    April
+    May
+    November
+    December
+)
+
+type geometry interface {
+    area() float64
+		perim() float64
+		changeSize(width, height float64)
+}
+
+type rect struct {
+    width, height float64
+}
+type circle struct {
+    radius float64
+}
+
+func (r rect) area() float64 {
+    return r.width * r.height
+}
+func (r rect) perim() float64 {
+    return 2*r.width + 2*r.height
+}
+func (r *rect) changeSize(width, height float64) {
+	r.width = width
+	r.height = height
+}
+// func (r *rect) changeSize(width, height float64) {
+// 	r.width = width
+// 	r.height = height
+// }
+
+func (c circle) area() float64 {
+    return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+    return 2 * math.Pi * c.radius
+}
+func (c *circle) changeSize(width, height float64) {
+}
+
+// meaure geometry and print size
+func measure(g geometry) {
+		g.changeSize(1, 2)
+    fmt.Println(g)
+    fmt.Println(g.area())
+    fmt.Println(g.perim())
+}
+
+func measureP(g geometry) {
+	g.changeSize(1, 2)
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
 func main() {
- fmt.Println(fmt.Println(quote.Hello()))
+	model.Demo()
+	fmt.Println(February)
+	// testgo.
+    // r := rect{width: 3, height: 4}
+		// c := circle{radius: 5}
+		// rn := new(rect)
+		
+		// fmt.Println(r)
+		// fmt.Println(c)
+		// fmt.Println(rn)
+    // measure(&r)
+		// measure(&c)
+		// measure(rn)
+		// fmt.Println(r)
+		// fmt.Println(c)
+		// fmt.Println(rn)
 }
---
+++ src/model/model.go
@@ -0,0 +1,16 @@
+package model
+
+import (
+	"fmt"
+)
+
+type Employee struct {
+    Id          int32
+    FirstName   string
+    LastName    string
+    BadgeNumber int32
+}
+
+func Demo()  {
+    fmt.Println("hello")
+}
