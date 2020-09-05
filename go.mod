372fa70a2b6d14c585fbabdb69c683dc5f8dd39f
King Wu <king.wuff101@gmail.com>
2020-09-06 01:21:22 +0800

    Try gqlgen

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module kw101/go-playground
 go 1.15
 
 require (
+	github.com/99designs/gqlgen v0.12.2
 	github.com/graph-gophers/graphql-go v0.0.0-20200819123640-3b5ddcd884ae
 	github.com/onsi/ginkgo v1.14.1
 	github.com/onsi/gomega v1.10.1
+	github.com/vektah/gqlparser/v2 v2.0.1
 	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import (
-	"kw101/go-playground/api/graphql"
-	"log"
-	"net/http"
+// import (
+// 	"kw101/go-playground/api/graphql"
+// 	"log"
+// 	"net/http"
 
-	graphqlGo "github.com/graph-gophers/graphql-go"
-	"github.com/graph-gophers/graphql-go/relay"
-)
+// 	graphqlGo "github.com/graph-gophers/graphql-go"
+// 	"github.com/graph-gophers/graphql-go/relay"
+// )
 
-func main() {
-	schema := graphqlGo.MustParseSchema(graphql.Schema, &graphql.Resolver{})
-	http.Handle("/api/v1", &relay.Handler{Schema: schema})
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+// func main() {
+// 	schema := graphqlGo.MustParseSchema(graphql.Schema, &graphql.Resolver{})
+// 	http.Handle("/api/v1", &relay.Handler{Schema: schema})
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
